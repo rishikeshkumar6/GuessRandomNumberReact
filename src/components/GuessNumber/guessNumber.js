c47a2c5c735cb6e760117cbc4de708f2e21bd7c7
rishikeshkumar6 <rishikeshkumarsingh810@gmail.com>
2023-04-27 20:58:14 +0530

    this is a react Guess number assignment

--- src/components/GuessNumber/guessNumber.js
+++ src/components/GuessNumber/guessNumber.js
@@ -3,18 +3,19 @@ import './guessnumber.css'
 export function GuessNumber(){
    
     const [number,setnumber]=useState()
+    const [randomNumber,setrandomNumber]=useState(1)
     function handleNumber(event){
           setnumber(event.target.value)
     }
     function Submission(){
-        const randomNumber=Math.floor(Math.random()*10)+20
+        setrandomNumber(Math.floor(Math.random()*10)+20)
         if(number<randomNumber){
             alert(' you guessed a smaller number .')
         }
         if(number>randomNumber){
             alert('you guessed a bigger number')
         }
-        if(number==randomNumber){
+        if(Number(number)===Number(randomNumber)){
             alert('Congratualation you guess right number')
         }
     }
@@ -22,6 +23,7 @@ export function GuessNumber(){
        <div className='parent'>
         <input type="number" onChange={handleNumber} className='number'/>
         <button onClick={Submission} class='btnstyle'>submit</button>
+        <h3>Random Number : {randomNumber}</h3>
         
        </div>
     )
